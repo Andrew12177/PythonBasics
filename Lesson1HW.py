92ac57ef7e8f28eb953da5c4b7000eff80c2981c
Andrew12177 <andrew12177@mail.ru>
2022-03-01 15:04:12 +0300

    Hi

--- Lesson1HW.py
+++ Lesson1HW.py
@@ -20,6 +20,19 @@
 # res = int(n) + int(n1) + int(n2)
 # print('The result is :', res)
 
+# n = abs(int(input('Введите целое положительное число: ')))
+# max = n % 10
+# while n >= 1:
+#     n = n // 10
+#     if n % 10 > max:
+#         max = n % 10
+#     if n > 9:
+#         continue
+#     else:
+#         print("Максимальное цифра в числе ", max)
+#         break
+
+
 
 
 
