82117d81ab12746ca25b4b30b91c389ed1f6cbaf
Andrew12177 <andrew12177@mail.ru>
2022-03-01 14:19:13 +0300

    Hi

---
+++ Lesson1HW.py
@@ -0,0 +1,25 @@
+# name = 'Boris'
+# print(name)
+# city = input('Иведите свой город проживания: ')
+# print(city)
+# age = int(input('Indicate your age: '))
+# print(age)
+# print(type(age))
+
+# seconds = int(input('Введите время: '))
+# hh = seconds // 3600
+# hh = hh % 24
+# mm = seconds // 60
+# mm = seconds % 60
+# sec = seconds % 60
+# print(f"{hh}:{mm}:{sec}")
+
+# n = input('Введите число n: ')
+# n1 = n + n
+# n2 = n + n + n
+# res = int(n) + int(n1) + int(n2)
+# print('The result is :', res)
+
+
+
+
